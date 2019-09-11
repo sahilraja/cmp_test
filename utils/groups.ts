a974b7ff744a9774c82632bb429561d94567b725
Sai Jeevan Balla <jeevan.balla@transerve.com>
2019-09-11 18:58:48 +0530

    added collaborators and viewers apis

--- utils/groups.ts
+++ utils/groups.ts
@@ -18,3 +18,58 @@ export async function groupsAddPolicy(userId: string, docId: string, role: strin
         throw err;
     };
 };
+
+
+// Add Policies for Documents
+export async function groupsRemovePolicy(userId: string, docId: string, role: string) {
+    try {
+        let Options = {
+            uri: `${process.env.GROUPS_URL}/document/user/remove`,
+            method: "POST",
+            body: {
+                userId: userId,
+                docId: docId,
+                role: role
+            },
+            json: true
+        }
+        return await request(Options);
+    } catch (err) {
+        throw err;
+    };
+};
+
+// Add Policies for Documents
+export async function groupsPolicyFilter(userId: string, filter: number = 0, policy: string = "p") {
+    try {
+        let Options = {
+            uri: `${process.env.GROUPS_URL}/policy/filter`,
+            method: "GET",
+            qs: {
+                userId: userId,
+                filter: filter,
+                policy: policy
+            },
+            json: true
+        }
+        return await request(Options);
+    } catch (err) {
+        throw err;
+    };
+};
+
+
+export async function GetIdsForDoc(docId: string, role: string) {
+    try {
+        let policies = await groupsPolicyFilter(`document/${docId}`, 1, "p")
+        if (!policies.data) throw new Error("policies not found for this document.")
+        let users = policies.data.filter((policy: string[]) => {
+            if (policy[2] = role)
+                return policy[0]
+        }).map((key: any) => key[0])
+        return [...new Set(users)]
+    } catch (err) {
+        throw err;
+    };
+};
+
