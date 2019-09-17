cce3af2f0c5d760b5aac9c1a2fc9dfaf794b72c0
Sai Jeevan Balla <jeevan.balla@transerve.com>
2019-09-17 19:19:32 +0530

    added validatinons and error message file

--- users/router.ts
+++ users/router.ts
@@ -1,161 +1,161 @@
 import { Router, Request, Response, Handler } from "express";
 import { inviteUser, user_list, edit_user as edit_user, user_status, user_login, userInviteResend, RegisterUser, userDetails, userRoles, userCapabilities, forgotPassword, setNewPassword, createGroup, editGroup, groupList, groupStatus, groupDetail, addMember, removeMembers, userSuggestions } from "./module";
 import { authenticate } from "../utils/utils";
+import { NextFunction } from "connect";
 var multer = require('multer')
 var upload = multer()
-
 const router = Router();
 
-//  Invite user
-router.post('/create', authenticate, async (req: any, res: any, next: any) => {
+//  Invite User
+router.post('/create', authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await inviteUser(req.body, res.locals.user));
     } catch (err) {
-        res.status(400).send({ error: err.message })
+        next(err);
     };
 });
 
 // Register User
-router.post("/register/:token", upload.single('uploadPhoto'), async (req: Request, res: Response, next: Handler) => {
+router.post("/register/:token", upload.single('uploadPhoto'), async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await RegisterUser(req.body, req.params.token, req.file))
     } catch (err) {
-        res.status(400).send({ error: err.message })
-    }
+        next(err);
+    };
 });
 
 
 //  Resend invite link
-router.get('/invite/resend/:role/:id', authenticate, async (req: Request, res: Response, next: Handler) => {
+router.get('/invite/resend/:role/:id', authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { role, id } = req.params
         res.status(200).send(await userInviteResend(id, role));
     } catch (err) {
-        res.status(400).send({ error: err.message })
+        next(err);
     };
 });
 
 //  user list
-router.get('/list', authenticate, async (req: Request, res: Response, next: Handler) => {
+router.get('/list', authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         req.query.page = req.query.page || 1;
         req.query.limit = 50;
         res.status(200).send(await user_list(req.query, res.locals.user.id, req.query.page, req.query.limit));
     } catch (err) {
-        res.status(400).send({ error: err.message })
+        next(err);
     };
 });
 
 //  Edit User
-router.get('/edit/:id', authenticate, async (req: Request, res: Response, next: Handler) => {
+router.get('/edit/:id', authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).send(await edit_user(req.params.id, req.body));
+        res.status(200).send(await edit_user(req.params.id, req.body, res.locals.user));
     } catch (err) {
-        res.status(400).send({ error: err.message })
+        next(err);
     };
 });
 
 //  change status user
-router.put('/status/:id', authenticate, async (req: Request, res: Response, next: Handler) => {
+router.put('/status/:id', authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).send(await user_status(req.params.id));
+        res.status(200).send(await user_status(req.params.id, res.locals.user.id));
     } catch (err) {
-        res.status(400).send({ error: err.message })
+        next(err);
     };
 });
 
 //  login user
-router.post('/email/login', async (req: Request, res: Response, next: Handler) => {
+router.post('/email/login', async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await user_login(req.body));
     } catch (err) {
-        res.status(400).send({ error: err.message })
+        next(err);
     };
 });
 
 // Get user Details
-router.get("/me", authenticate, async (req: any, res: Response, next: Handler) => {
+router.get("/me", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await userDetails(res.locals.user.id));
     } catch (err) {
-        res.status(400).send({ err: err.message });
+        next(err)
     };
 });
 
 // Get user roles
-router.get("/me/role", authenticate, async (req: any, res: Response, next: Handler) => {
+router.get("/me/role", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await userRoles(res.locals.user.id));
     } catch (err) {
-        res.status(400).send({ err: err.message });
+        next(err);
     };
 });
 
-// Get user roles
-router.get("/me/capabilities", authenticate, async (req: any, res: Response, next: Handler) => {
+// Get user capabilities
+router.get("/me/capabilities", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await userCapabilities(res.locals.user.id));
     } catch (err) {
-        res.status(400).send({ err: err.message });
+        next(err);
     };
 });
 
 // Forgot Password
-router.post("/forgot", async (req: any, res: Response, next: Handler) => {
+router.post("/forgot", async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await forgotPassword(req.body));
     } catch (err) {
-        res.status(400).send({ err: err.message });
+        next(err)
     };
 });
 
 // Set New Password
-router.post("/forgot/password/:token", async (req: any, res: Response, next: Handler) => {
+router.post("/forgot/password/:token", async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await setNewPassword(req.body, req.params.token));
     } catch (err) {
-        res.status(400).send({ err: err.message });
+        next(err);
     };
 });
 
 //  Add Group
-router.post("/group/create", authenticate, async (req: any, res: any, next: any) => {
+router.post("/group/create", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await createGroup(req.body))
     } catch (err) {
-        next(err)
-    }
+        next(err);
+    };
 });
 
 //  List Group
-router.get("/group/list", authenticate, async (req: any, res: any, next: any) => {
+router.get("/group/list", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await groupList())
     } catch (err) {
-        next(err)
-    }
+        next(err);
+    };
 });
 
 //  Edit Group
-router.put("/group/:id/edit", authenticate, async (req: any, res: any, next: any) => {
+router.put("/group/:id/edit", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await editGroup(req.body, req.params.id))
     } catch (err) {
-        next(err)
-    }
+        next(err);
+    };
 });
 
-//  edit status of city code
-router.put("/group/:id/status", authenticate, async (req: any, res: any, next: any) => {
+//  edit status of group
+router.put("/group/:id/status", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await groupStatus(req.params.id))
     } catch (err) {
-        next(err)
-    }
+        next(err);
+    };
 });
 
 //  Add Member
-router.post("/group/:id/member/add", authenticate, async (req: any, res: any, next: any) => {
+router.post("/group/:id/member/add", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await addMember(req.params.id, req.body.users));
     } catch (err) {
@@ -164,7 +164,7 @@ router.post("/group/:id/member/add", authenticate, async (req: any, res: any, ne
 });
 
 //  Remove Member
-router.post("/group/:id/member/remove", authenticate, async (req: any, res: any, next: any) => {
+router.post("/group/:id/member/remove", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await removeMembers(req.params.id, req.body.users));
     } catch (err) {
@@ -172,8 +172,8 @@ router.post("/group/:id/member/remove", authenticate, async (req: any, res: any,
     };
 });
 
-//  user group list
-router.get("/suggestion", authenticate, async (req: any, res: any, next: any) => {
+//  Get User suggestion
+router.get("/suggestion", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await userSuggestions(req.query.search));
     } catch (err) {
@@ -182,7 +182,7 @@ router.get("/suggestion", authenticate, async (req: any, res: any, next: any) =>
 });
 
 //  Group Details
-router.get("/group/:id", authenticate, async (req: any, res: any, next: any) => {
+router.get("/group/:id", authenticate, async (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).send(await groupDetail(req.params.id));
     } catch (err) {
--- utils/error_msg.ts
+++ utils/error_msg.ts
@@ -1,6 +1,40 @@
-export const userRouter = {
+export const USER_ROUTER = {
+    INVALID_ADMIN: "User don't have capability to perform this action.",
+    CREATE_ROLE_FAIL: "Fail to create role.",
+    TOKEN_MISSING: "Missing Token.",
+    TOKEN_INVALID: "Invalid Token.",
+    MANDATORY: "Required all mandatory fields.",
+    VALID_PHONE_NO: "Enter Valid Phone Number.",
+    VALID_PASSWORD: "Password must have one special character and at least 6 characters",
+    ALREADY_REGISTER: "Already User Registered.",
+    INVALID_PARAMS_ID: "Given id not Valid.",
+    USER_NOT_EXIST: "User Not found.",
+    INVALID_FIELDS: "Enter Valid input.",
+    USER_NOT_REGISTER: "User Not Registered.",
+    INVALID_LOGIN_DETAILS: "Invalid login details.",
+    DEACTIVATED_BY_ADMIN: "Account Deactivated By Admin.",
+    ROLE_NOT_FOUND: "Fail to get Role.",
+    CAPABILITIES_NOT_FOUND: "Fail to get Capabilities.",
+    EMAIL_VERIFIED: "Email Already Verified",
+    GROUP_NOT_FOUND: "Group not found.",
+    USER_ARRAY: "Users must be an Array."
 
 };
+
+
+export const MAIL_SUBJECT = {
+    INVITE_USER: "Invitation from CITIIS Management Platform",
+    FORGOT_PASSWORD: "CMP Reset password instructions"
+
+};
+
+export const RESPONSE = {
+    SUCCESS_EMAIL: "Email send successfully.",
+    ACTIVE: "active",
+    INACTIVE: "inactive",
+    ADD_MEMBER: "Successfully Added Member."
+    REMOVE_MEMBER: "Successfully Removed Member."
+}
 
 export const GLOBAL_SCOPE = "global";
 
