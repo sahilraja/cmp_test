9219fc1991f34f3f23eddc2fb4c188496ecdcbd3
Sai Jeevan Balla <jeevan@webileapps.com>
2019-11-05 19:31:26 +0530

    validations in all modules

--- project/module.ts
+++ project/module.ts
@@ -7,7 +7,7 @@ import { userRoleAndScope, roles_list, role_list } from "../role/module";
 import { taskModel } from "./task_model";
 import { workflowModel } from "./workflow_model";
 import { checkCapability } from "../utils/rbac";
-import { httpRequest } from "../utils/role_management";
+import { httpRequest, checkRoleScope } from "../utils/role_management";
 import { TASKS_URL } from "../utils/urls";
 import { getUserDetail } from "../users/module";
 import { userFindMany } from "../utils/users";
@@ -19,6 +19,8 @@ export async function createProject(reqObject: any, user: any) {
     if (!reqObject.reference || !reqObject.name) {
       throw new Error(MISSING);
     }
+    let isEligible = await checkRoleScope(user.role, "create-project");
+    if (!isEligible) throw new APIError("Unauthorized Action.", 403);
     //  check capability
     // let capability = await checkCapability({
     //   role: user.role,
@@ -29,7 +31,7 @@ export async function createProject(reqObject: any, user: any) {
 
     return await ProjectSchema.create({
       createdBy: user._id,
-      name:reqObject.name,
+      name: reqObject.name,
       reference: reqObject.reference,
       city: reqObject.cityname,
       summary: reqObject.description || "N/A",
@@ -48,6 +50,8 @@ export async function editProject(id: any, reqObject: any, user: any) {
     if (!id || !user) throw new Error(MISSING);
     let obj: any = {};
 
+    let isEligible = await checkRoleScope(user.role, "edit-project");
+    if (!isEligible) throw new APIError("Unauthorized Action.", 403);
     //  check capability
     // let capability = await checkCapability({
     //   role: user.role,
@@ -68,19 +72,19 @@ export async function editProject(id: any, reqObject: any, user: any) {
     if (reqObject.description) {
       obj.summary = reqObject.description;
     }
-    if(reqObject.maturationStartDate){
+    if (reqObject.maturationStartDate) {
       obj.maturationStartDate = { date: reqObject.maturationStartDate, modifiedBy: user._id }
     }
-    return await ProjectSchema.findByIdAndUpdate(id, { $set:obj }, { new: true }).exec();
+    return await ProjectSchema.findByIdAndUpdate(id, { $set: obj }, { new: true }).exec();
   } catch (err) {
     console.error(err);
     throw err;
   }
 }
 
-export async function manageProjectMembers(id: string, members:string[], loggedUserId: string) {
+export async function manageProjectMembers(id: string, members: string[], loggedUserId: string) {
   members = Array.from(new Set(members))
-  if(members.includes(loggedUserId)){
+  if (members.includes(loggedUserId)) {
     throw new APIError(`You are trying to add yourself as project member`)
   }
   return await ProjectSchema.findByIdAndUpdate(id, { $set: { members } }, { new: true }).exec()
@@ -91,9 +95,9 @@ export async function getProjectMembers(id: string) {
   const [users, formattedRoleObjs]: any = await Promise.all([
     userFindMany('_id', members, { firstName: 1, lastName: 1, middleName: 1, email: 1 }),
     role_list()
-  ]) 
+  ])
   const usersRoles = await Promise.all(members.map((user: string) => userRoleAndScope(user)))
-  return users.map((user:any, i:number) => ({...user, role: formatUserRole((usersRoles.find((role:any) => role.user == user._id) as any).data.global[0], formattedRoleObjs.roles)}))
+  return users.map((user: any, i: number) => ({ ...user, role: formatUserRole((usersRoles.find((role: any) => role.user == user._id) as any).data.global[0], formattedRoleObjs.roles) }))
 }
 
 function formatUserRole(role: string, formattedRoleObjs: any) {
@@ -112,16 +116,18 @@ export async function projectList() {
 }
 
 //  edit status of city code
-export async function city_code_status(id: any) {
+export async function city_code_status(id: any, userObj: any) {
   try {
     if (!id) throw new Error(MISSING);
+    let isEligible = await checkRoleScope(userObj.role, "edit-project");
+    if (!isEligible) throw new APIError("Unauthorized Action.", 403);
     let projectData: any = await ProjectSchema.findById(id).exec();
     if (!projectData) {
       throw new Error("project not there");
     }
     return await ProjectSchema.findByIdAndUpdate(
       { id },
-      { is_active: projectData.is_active ? false : true },{new: true}
+      { is_active: projectData.is_active ? false : true }, { new: true }
     ).exec();
   } catch (err) {
     console.error(err);
@@ -130,11 +136,13 @@ export async function city_code_status(id: any) {
 }
 
 //  add tag
-export async function add_tag(reqObject: any) {
+export async function add_tag(reqObject: any, userObj: any) {
   try {
     if (!reqObject.tag) {
       throw new Error(MISSING);
     }
+    let isEligible = await checkRoleScope(userObj.role, "create-tag");
+    if (!isEligible) throw new APIError("Unauthorized Action.", 403);
     return await tags.create({
       tag: reqObject.tag,
       description: reqObject.description || "N/A"
@@ -146,8 +154,10 @@ export async function add_tag(reqObject: any) {
 }
 
 //  edit tag
-export async function edit_tag(id: any, reqObject: any) {
+export async function edit_tag(id: any, reqObject: any, userObj: any) {
   try {
+    let isEligible = await checkRoleScope(userObj.role, "edit-tag");
+    if (!isEligible) throw new APIError("Unauthorized Action.", 403);
     let obj: any = {};
     if (reqObject.tag) {
       obj.tag = reqObject.tag;
@@ -164,13 +174,15 @@ export async function edit_tag(id: any, reqObject: any) {
 
 
 
-export async function getTagByIds(ids:string[]) {
-  return await tags.find({_id:{$in:ids}}).exec()
+export async function getTagByIds(ids: string[]) {
+  return await tags.find({ _id: { $in: ids } }).exec()
 }
 
 //  edit status of tag
-export async function tag_status(id: any) {
+export async function tag_status(id: any, userObj: any) {
   try {
+    let isEligible = await checkRoleScope(userObj.role, "edit-tag");
+    if (!isEligible) throw new APIError("Unauthorized Action.", 403);
     let city: any = await tags.findById(id);
     if (!city) {
       throw new Error(MISSING);
@@ -250,7 +262,7 @@ export async function getProjectsList(userId: any, userToken: string) {
   try {
     // let userProjects: any = await userRoleAndScope(userId);
     // if (!userProjects) throw new Error("user have no projects");
-    const {docs:list, page, pages} = await ProjectSchema.paginate({$or:[{createdBy: userId}, {members:{$in:[userId]}}]})
+    const { docs: list, page, pages } = await ProjectSchema.paginate({ $or: [{ createdBy: userId }, { members: { $in: [userId] } }] })
     const projectIds = (list || []).map((_list) => _list.id)
     return { docs: await mapProgressPercentageForProjects(projectIds, userToken, list), page, pages }
   } catch (error) {
@@ -262,14 +274,14 @@ export async function getProjectsList(userId: any, userToken: string) {
 async function mapProgressPercentageForProjects(projectIds: string[], userToken: string, list: any[]) {
   const projectRelatedTasks = await httpRequest({
     url: `${TASKS_URL}/task/getTasksByProjectIds`,
-    body:{projectIds},
+    body: { projectIds },
     json: true,
-    method:'POST',
-    headers: {'Authorization': `Bearer ${userToken}`}
+    method: 'POST',
+    headers: { 'Authorization': `Bearer ${userToken}` }
   })
-  return (list || []).map((_list) => { 
+  return (list || []).map((_list) => {
     const tasksForTheProject = (projectRelatedTasks as any).filter((task: any) => task.projectId == _list.id)
-    return ({ ..._list.toJSON(), progressPercentage: tasksForTheProject.reduce((p: number, c: any) => p + (c.progressPercentage || 0), 0)/tasksForTheProject.length })
+    return ({ ..._list.toJSON(), progressPercentage: tasksForTheProject.reduce((p: number, c: any) => p + (c.progressPercentage || 0), 0) / tasksForTheProject.length })
   })
 }
 
@@ -283,20 +295,22 @@ export async function getProjectDetail(projectId: string) {
   };
 };
 
-export async function createTask(payload:any, projectId: string, userToken: string){
+export async function createTask(payload: any, projectId: string, userToken: string, userObj: any) {
+  let isEligible = await checkRoleScope(userObj.role, "project-create-task");
+  if (!isEligible) throw new APIError("Unauthorized Action.", 403);
   const taskPayload = await formatTaskPayload(payload, projectId)
   const options = {
     url: `${TASKS_URL}/task/create`,
-    body: {...taskPayload},
-    method:'POST',
-    headers: {'Authorization':`Bearer ${userToken}`},
+    body: { ...taskPayload },
+    method: 'POST',
+    headers: { 'Authorization': `Bearer ${userToken}` },
     json: true
   }
   return await httpRequest(options)
 }
 
 async function formatTaskPayload(payload: any, projectId: string) {
-  return {...payload, projectId}
+  return { ...payload, projectId }
   const projectInfo: any = await ProjectSchema.findById(projectId).exec()
   const memberRoles = await Promise.all(
     projectInfo.members.map((member: string) => userRoleAndScope(member))
@@ -306,9 +320,9 @@ async function formatTaskPayload(payload: any, projectId: string) {
   let viewers = []
   let supporters = []
   let assignee
-  if(payload.assignee && !Types.ObjectId.isValid(payload.assignee)){
+  if (payload.assignee && !Types.ObjectId.isValid(payload.assignee)) {
     const filteredAssignees = memberRoles.filter((role: any) => (role.data.global[0] == payload.assignee))
-    if(filteredAssignees.length > 1){
+    if (filteredAssignees.length > 1) {
       throw new APIError(PROJECT_ROUTER.MORE_THAN_ONE_RESULT_FOUND)
     } else {
       assignee = filteredAssignees[0]
@@ -326,76 +340,78 @@ async function formatTaskPayload(payload: any, projectId: string) {
 export async function getProjectTasks(projectId: string, userToken: string) {
   const options = {
     url: `${TASKS_URL}/task/getTasksByProject`,
-    body: {projectId},
-    headers: {'Authorization':`Bearer ${userToken}`},
-    method:'POST',
+    body: { projectId },
+    headers: { 'Authorization': `Bearer ${userToken}` },
+    method: 'POST',
     json: true
   }
   return await httpRequest(options)
 }
 
-export async function editTask(projectId: string, taskId: string, userId: string, userToken: string, payload: any) {
+export async function editTask(projectId: string, taskId: string, userObj: any, userToken: string, payload: any) {
+  let isEligible = await checkRoleScope(userObj.role, "project-edit-task");
+  if (!isEligible) throw new APIError("Unauthorized Action.", 403);
   const projectDetail: any = await ProjectSchema.findById(projectId).exec()
-  if(!projectDetail){
+  if (!projectDetail) {
     throw new Error(PROJECT_ROUTER.PROJECT_NOT_EXISTS)
   }
-  if(!((projectDetail as any).members || []).includes(userId)){
+  if (!((projectDetail as any).members || []).includes(userObj._id)) {
     throw new Error(PROJECT_ROUTER.NOT_MEMBER_OF_PROJECT)
   }
   const options = {
     url: `${TASKS_URL}/task/${taskId}/soft-edit`,
     body: payload,
-    method:'POST',
-    headers: {'Authorization':`Bearer ${userToken}`},
+    method: 'POST',
+    headers: { 'Authorization': `Bearer ${userToken}` },
     json: true
   }
   return await httpRequest(options)
 }
 
 export async function linkTask(projectId: string, taskId: string, userToken: string) {
-  if(!taskId){
+  if (!taskId) {
     throw new Error(PROJECT_ROUTER.TASK_REQUIRED_FOR_LINKING)
   }
   const options = {
     url: `${TASKS_URL}/task/${taskId}/soft-edit`,
-    body: {projectId},
-    method:'POST',
-    headers: {'Authorization':`Bearer ${userToken}`},
+    body: { projectId },
+    method: 'POST',
+    headers: { 'Authorization': `Bearer ${userToken}` },
     json: true
   }
-  return await httpRequest(options)  
+  return await httpRequest(options)
 }
 
-export async function ganttChart(projectId: string, userToken: string){
+export async function ganttChart(projectId: string, userToken: string) {
   const projectDetail = await ProjectSchema.findById(projectId).exec()
   const options = {
     url: `${TASKS_URL}/task/getTasksWithSubTasks`,
-    method:'POST',
-    body:{projectIds:[projectId]},
-    headers: {'Authorization':`Bearer ${userToken}`},
+    method: 'POST',
+    body: { projectIds: [projectId] },
+    headers: { 'Authorization': `Bearer ${userToken}` },
     json: true
   }
-  const tasks = await httpRequest(options)  
-  return {...(projectDetail as any).toJSON(), tasks}
+  const tasks = await httpRequest(options)
+  return { ...(projectDetail as any).toJSON(), tasks }
 }
 
 export async function projectMembers(id: string) {
   let [project, formattedRoleObjs]: any = await Promise.all([
     ProjectSchema.findById(id).exec(),
     role_list()
-  ]) 
+  ])
   if (!project) throw new Error("Project Not Found.");
   const userIds = [...project.members, project.createdBy]
-  const usersRoles = await Promise.all(userIds.map((userId:string) => userRoleAndScope(userId) ))
-  return userIds.map((user:any, i:number) => ({
-    value: user, 
-    key: formatUserRole((usersRoles.find((role:any) => role.user == user) as any).data.global[0], formattedRoleObjs.roles)
+  const usersRoles = await Promise.all(userIds.map((userId: string) => userRoleAndScope(userId)))
+  return userIds.map((user: any, i: number) => ({
+    value: user,
+    key: formatUserRole((usersRoles.find((role: any) => role.user == user) as any).data.global[0], formattedRoleObjs.roles)
   }))
 }
 
 export async function getTaskDetail(projectId: string, id: string, userId: string, userToken: string) {
   const projectDetail: any = await ProjectSchema.findById(projectId).exec()
-  if(!projectDetail.members.includes(userId) && projectDetail.createdBy != userId){
+  if (!projectDetail.members.includes(userId) && projectDetail.createdBy != userId) {
     throw new APIError(`You dont have access to this project`)
   }
   const options = {
