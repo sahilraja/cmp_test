9551dc78122dbb6c0f00ae6f6b725c404c848e0c
Saikumar <saikumar@webileapps.com>
2019-11-07 18:09:55 +0530

    fixed issue in task logs get

--- log/router.ts
+++ log/router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { OK } from "http-status-codes";
 import { paginatedList, create, list, getTaskLogs } from "./module";
 import { APIError } from "../utils/custom-error";
+import { authenticate } from "../utils/utils";
 const router = Router()
 
 router.post('/create', async (req, res, next) => {
@@ -28,9 +29,9 @@ router.post(`/paginated-list`, async (req, res, next) => {
     }
 })
 
-router.get(`/get-task-logs`, async (req, res, next) => {
+router.get(`/get-task-logs`, authenticate, async (req, res, next) => {
     try {
-        res.status(OK).send(await getTaskLogs(req.query.taskId))
+        res.status(OK).send(await getTaskLogs(req.query.taskId, (req as  any).token))
     } catch (error) {
         next(new APIError(error.message))
     }
