96029d5462f582a2de0a0b1178fa873f1819b946
@eswar.bonthu <eswar.b@transerve.com>
2019-11-13 13:13:54 +0530

    added displayName  to template

--- email-templates/model.ts
+++ email-templates/model.ts
@@ -1,12 +1,13 @@
-import {Schema,model} from "mongoose";
+import { Schema, model } from "mongoose";
 import * as mongoosePaginate from "mongoose-paginate";
 
 const schema = new Schema({
     content: { type: String },
-    templateName: { type: String, unique: true},
+    templateName: { type: String, unique: true },
+    displayName: { type: String },
     subject: { type: String },
-    form:{type: String, default: 'html'}
-    },{ timestamps: true }
+    form: { type: String, default: 'html' }
+}, { timestamps: true }
 );
 schema.plugin(mongoosePaginate);
 export const TemplateSchema = model("template", schema);
