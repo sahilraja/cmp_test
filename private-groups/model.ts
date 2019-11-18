6025bb2bc1ab1cdefb6af452ae7cfbcd87f6dc8a
Sai Jeevan Balla <jeevan@webileapps.com>
2019-11-18 20:38:50 +0530

    removed index in private groups

--- private-groups/model.ts
+++ private-groups/model.ts
@@ -9,6 +9,5 @@ const schema = new Schema({
     is_active: { type: Boolean, default: true }
 }, { timestamps: true });
 
-schema.index({ name: 1, createdBy: 1 }, { unique: true })
 schema.plugin(mongoosePagination);
 export const privateGroupSchema = model("private_group", schema);
