2a4432fcb1d8d7affd390c55b5f21e718d1b2848
Saikumar <saikumar@webileapps.com>
2019-11-22 19:34:26 +0530

    fixed risk bugs

--- project/module.ts
+++ project/module.ts
@@ -684,7 +684,7 @@ export async function deleteUtilizedFund(projectId: string, payload: any, user: 
   const { document, cost, _id } = payload
   let updates: any = {}
   updates = { ...updates, modifiedAt: new Date(), modifiedBy: user._id }
-  updates['fundsReleased.$.deleted'] = true
+  updates['fundsUtilised.$.deleted'] = true
   const updatedProject: any = await ProjectSchema.findOneAndUpdate({ _id: projectId, 'fundsUtilised._id': _id }, { $set: updates }).exec()
   // createLog({activityType: ACTIVITY_LOG.UPDATED_FUND_UTILIZATION, projectId, oldCost: updatedProject.cost, updatedCost: payload.cost, activityBy: userId})
   return updatedProject
