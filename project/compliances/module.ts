716cd7b4db0b6dc87aa1aa1646724532cf14ae98
Sai Jeevan Balla <jeevan@webileapps.com>
2019-12-11 14:15:19 +0530

    modified miscomplience list api

--- project/compliances/module.ts
+++ project/compliances/module.ts
@@ -6,17 +6,17 @@ import { TASKS_URL } from "../../utils/urls";
 
 export async function createCompliance(payload: any, projectId: string, userObj: any) {
     const isEligible = await checkRoleScope(userObj.role, 'create-compliance')
-    if(!isEligible){
+    if (!isEligible) {
         throw new APIError(COMPLIANCES.UNAUTHORIZED_TO_CREATE)
     }
-    if(!payload.taskId){
+    if (!payload.taskId) {
         throw new APIError(COMPLIANCES.REQUIRED_TASK)
     }
-    return await ComplianceSchema.create({...payload, projectId, createdBy: userObj._id})
+    return await ComplianceSchema.create({ ...payload, projectId, createdBy: userObj._id })
 }
 
 export async function listCompliances(userToken: string, projectId: string) {
-    const compliances = await ComplianceSchema.find({projectId}).exec()
+    const compliances = await ComplianceSchema.find({ projectId }).exec()
     const taskIds = compliances.map((compliance: any) => compliance.taskId).filter(v => !!v)
     const tasks: any = await httpRequest({
         url: `${TASKS_URL}/task/getTasksDocs`,
@@ -25,15 +25,29 @@ export async function listCompliances(userToken: string, projectId: string) {
         headers: { 'Authorization': `Bearer ${userToken}` },
         json: true
     })
-    return compliances.map((compliance: any) => ({ ...compliance.toJSON(), task: tasks.find((task: any) => task._id == compliance.taskId), taskStatus: tasks.find((task: any) => task._id == compliance.taskId).status }))
+    let complianceData = compliances.map((compliance: any) => {
+        return {
+            ...compliance.toJSON(),
+            task: tasks.find((task: any) => task._id == compliance.taskId),
+            taskStatus: tasks.find((task: any) => task._id == compliance.taskId).status
+        }
+    })
+    let SpvCompliance = complianceData.filter(({complianceType})=> complianceType == "SPV")
+    let ProjectCompliance = complianceData.filter(({complianceType})=> complianceType == "PROJECT")
+    return {
+        SpvPercentage: SpvCompliance? ((SpvCompliance.filter(({taskStatus})=> taskStatus == 4 || taskStatus == 5)).length/SpvCompliance.length)/100: 0,
+        ProjectPercentage: ProjectCompliance? ((ProjectCompliance.filter(({taskStatus})=> taskStatus == 4 || taskStatus == 5)).length/ProjectCompliance.length)/100: 0,
+        SpvCompliance, ProjectCompliance
+    }
 }
+
 
 export async function editCompliance(id: string, updates: any, userObj: any) {
     const isEligible = await checkRoleScope(userObj.role, 'edit-compliance')
-    if(!isEligible){
+    if (!isEligible) {
         throw new APIError(COMPLIANCES.UNAUTHORIZED_TO_EDIT)
     }
-    if(Object.keys(updates).includes('taskId') && !updates.taskId){
+    if (Object.keys(updates).includes('taskId') && !updates.taskId) {
         throw new APIError(COMPLIANCES.REQUIRED_TASK)
     }
     // Need to handle task case
