f7e9f5b87c4adebb837aa06005467414bfa31338
Sai Jeevan Balla <jeevan@webileapps.com>
2019-12-12 17:42:30 +0530

    added permission to phase permissions

--- phase/module.ts
+++ phase/module.ts
@@ -2,66 +2,71 @@ import { phaseSchema } from "./model";
 import { APIError } from "../utils/custom-error";
 import { userDetails } from "../users/module";
 import { USER_ROUTER } from "../utils/error_msg";
+import { checkRoleScope } from "../utils/role_management";
 
 
-export async function createPhase(payload: any,createdBy:string) {
-    try{
-        if(!payload.phaseName && !payload.colorCode){
+export async function createPhase(payload: any, userObj: any) {
+    try {
+        let isEligible = await checkRoleScope(userObj.role, "phase-manage");
+        if (!isEligible) throw new APIError("Unauthorized Action.", 403);
+        if (!payload.phaseName && !payload.colorCode) {
             throw new APIError(USER_ROUTER.MANDATORY)
         }
-        let phaseInfo:any = await phaseSchema.create({...payload,createdBy});
-        let {disable,...phaseResult} =phaseInfo.toObject();
+        let phaseInfo: any = await phaseSchema.create({ ...payload, createdBy: userObj._id });
+        let { disable, ...phaseResult } = phaseInfo.toObject();
         return phaseResult
     }
-    catch(err){
+    catch (err) {
         throw err
     }
 }
 
-export async function editPhase(phaseId: string,body: any) {
-    try{
-        let phaseInfo:any = await phaseSchema.findByIdAndUpdate(phaseId, { $set: {phaseName:body.phaseName,colorCode:body.colorCode}},{new:true}).exec()
-        let {disable,...phaseResult} =phaseInfo.toObject();
+export async function editPhase(phaseId: string, body: any, userObj: any) {
+    try {
+        let isEligible = await checkRoleScope(userObj.role, "phase-manage");
+        if (!isEligible) throw new APIError("Unauthorized Action.", 403);
+        let phaseInfo: any = await phaseSchema.findByIdAndUpdate(phaseId, { $set: { phaseName: body.phaseName, colorCode: body.colorCode } }, { new: true }).exec()
+        let { disable, ...phaseResult } = phaseInfo.toObject();
         return phaseResult
     }
-    catch(err){
+    catch (err) {
         throw err
     }
 }
 
-export async function getPhase(phaseId:string) {
-    try{
-        let phaseInfo:any = await phaseSchema.findById(phaseId).exec();
-        let {disable,...phaseResult} =phaseInfo.toObject();
+export async function getPhase(phaseId: string) {
+    try {
+        let phaseInfo: any = await phaseSchema.findById(phaseId).exec();
+        let { disable, ...phaseResult } = phaseInfo.toObject();
         return phaseResult
     }
-    catch(err){
+    catch (err) {
         throw err
     }
 }
 
-export async function deletePhase(phaseId:string) {
-    try{
-        await phaseSchema.findByIdAndUpdate(phaseId,{disable:true}).exec();
-        return {messsage : "successfully deleted phase"}
+export async function deletePhase(phaseId: string) {
+    try {
+        await phaseSchema.findByIdAndUpdate(phaseId, { disable: true }).exec();
+        return { messsage: "successfully deleted phase" }
     }
-    catch(err){
+    catch (err) {
         throw err
     }
 }
 export async function listPhase() {
-    try{
+    try {
         return await phaseSchema.find({}).exec();
     }
-    catch(err){
+    catch (err) {
         throw err
     }
 }
-export async function userListPhase(userId:string){
-    try{
-        return await phaseSchema.findById({createdBy:userId}).exec();
+export async function userListPhase(userId: string) {
+    try {
+        return await phaseSchema.findById({ createdBy: userId }).exec();
     }
-    catch(err){
+    catch (err) {
         throw err
     }
 }
