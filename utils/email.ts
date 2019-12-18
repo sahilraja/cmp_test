8cbc12e7cfef8f6f7f49d36d0e8cc21f5c2f46a0
Saikumar <saikumar@webileapps.com>
2019-12-18 20:07:03 +0530

    changed config email

--- utils/email.ts
+++ utils/email.ts
@@ -6,8 +6,8 @@ var smtpTransport = require('nodemailer-smtp-transport');
 // const EMAIL = process.env.EMAIL || "cmp@niua.org";
 // const PASSWORD = process.env.PASSWORD || "hahahaha"
 
-const EMAIL = process.env.EMAIL || 'jeevan.balla@transerve.com';
-const PASSWORD = process.env.PASSWORD || '7989238348';
+const EMAIL = process.env.EMAIL || 'saikumar.b@transerve.com';
+const PASSWORD = process.env.PASSWORD || 'sai@transerve.001';
 
 // let transport = process.env.EMAIL ? smtpTransport({
 //   host: 'smtp.rediffmailpro.com',
