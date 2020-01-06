f2f56d8f5be65071c43f40192c7804d811d3aa6d
Saikumar <saikumar@webileapps.com>
2020-01-06 12:22:35 +0530

    removed trend Math.abs to show -ve values

--- risks/module.ts
+++ risks/module.ts
@@ -71,7 +71,7 @@ async function saveaAll(riskObj: any, projectId: string, userObj: any) {
                     riskObj.riskTrend = 0
                 } else {
                     let lastUpdatedObj: any = (await RiskSchema.find({ parentId: riskObj._id || riskObj.id })).pop()
-                    riskObj.riskTrend = Math.abs((lastUpdatedObj.impact || 0) * (lastUpdatedObj.probability || 0) - (riskObj.impact || 0) * (riskObj.probability || 0))
+                    riskObj.riskTrend = (lastUpdatedObj.impact || 0) * (lastUpdatedObj.probability || 0) - (riskObj.impact || 0) * (riskObj.probability || 0)
                 }
                 if (Object.keys(riskObj).includes('impact'))
                     if (oldObject.impact != riskObj.impact)
