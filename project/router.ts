01403a8269610cf5d206a61b6d4c85d9e04d923b
Saikumar <saikumar@webileapps.com>
2020-01-07 15:54:43 +0530

    download api change

--- project/router.ts
+++ project/router.ts
@@ -33,38 +33,6 @@ router.post("/create", async (req, res, next) => {
     }
 });
 
-router.post(`/upload-sample-excel`, async (req, res, next) => {
-    try {
-        let payload: any
-        const contentType: any = req.get('content-type');
-        if (contentType.includes('multipart/form-data')) {
-            payload = await uploadToFileService(req)
-        }
-        payload = JSON.parse(payload)
-        res.status(OK).send(await UploadFormatSchema.findOneAndUpdate({type:'TASK_EXCEL'},{type:'TASK_EXCEL', name:payload.name, fileId: payload.id}, {upsert: true}).exec())
-    } catch (error) {
-        next(new APIError(error.message))
-    }
-})
-
-router.get(`/download-bulk-task-excel`, async (request, response, next) => {
-    const detail: any = await UploadFormatSchema.findOne({ type: 'TASK_EXCEL' }).exec()
-    const req = (FILES_SERVER_BASE as string).startsWith("https") ?
-        httpsGet(`${FILES_SERVER_BASE}/files/${detail.fileId}`, (res: any) => {
-            response.setHeader('Content-disposition', `attachment;filename=${detail.name || `sample.csv`}`);
-            response.setHeader('Content-type', res.headers['content-type'])
-            res.pipe(response);
-        }) : httpGet(`${FILES_SERVER_BASE}/files/${detail.fileId}`, (res: any) => {
-            response.setHeader('Content-disposition', `attachment;filename=${detail.name || `sample.csv`}`);
-            response.setHeader('Content-type', res.headers['content-type'])
-            res.pipe(response);
-        });
-    req.on('error', (e: Error) => {
-        next(e);
-    });
-    req.end();
-})
-
 router.get(`/get-states`, async (req, res, next) => {
     try {
         res.status(OK).send(await getStates())
