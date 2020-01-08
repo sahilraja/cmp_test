ae94a470848fb8f4e4f038580275fbeb31dfb39b
Sai Jeevan Balla <jeevan@webileapps.com>
2020-01-08 15:31:21 +0530

    modifed web notification

--- utils/web-notification-messages.ts
+++ utils/web-notification-messages.ts
@@ -1,8 +1,8 @@
 export const DOC_NOTIFICATIONS = {
     inviteForDocument: (name: string) => `You are invited by [from] to collaborate on the document ${name}`,
     suggestTagNotification: (name: string) => `You have a tag suggestion on the document ${name} from [from]`,
-    approveTagNotification: (name: string) => `A document tag you suggested has been approved on the document ${name}`,
-    rejectTagNotification: (name: string) => `A document tag you suggested has been rejected on the document ${name}`,
+    approveTagNotification: (name: string) => `A tag you suggested has been approved on the document ${name}`,
+    rejectTagNotification: (name: string) => `A tag you suggested has been rejected on the document ${name}`,
     addCommentToDoc: (name: string) => `A new comment has been added to the document ${name}`,
     documentUpdate: (text: string, name: string) => `Document ${name}, has been updated ${text ? "with " + text : ""}`,
     invitePeopleDoc: (sharedUsers: string, role: string, name: string) => `Your document ${name} on CITIIS Management Platform, is shared with ${sharedUsers} in the capacity of ${role}`,
