605f7d6878bf39c20ff1f1a1905dbf4749c0b763
Saikumar <saikumar@webileapps.com>
2020-01-17 18:59:46 +0530

    commented validations

--- utils/web-notification-messages.ts
+++ utils/web-notification-messages.ts
@@ -2,15 +2,15 @@ export const DOC_NOTIFICATIONS = {
     inviteForDocument: (name: string) => `You are invited by [from] to collaborate on the document ${name}`,
     suggestTagNotification: (name: string) => `You have a tag suggestion on the document ${name} from [from]`,
     approveTagNotification: (name: string) => `A tag you suggested has been approved on the document ${name}`,
-    approveRemoveTagNotification: (name: string) => `A tag you suggested for remove has been approved on the document ${name}`,
+    approveRemoveTagNotification: (name: string) => `A tag you suggested for removal has been approved on the document ${name}`,
     rejectTagNotification: (name: string) => `A tag you suggested has been rejected on the document ${name}`,
-    rejectRemoveTagNotification: (name: string) => `A tag you suggested for remove has been rejected on the document ${name}`,
+    rejectRemoveTagNotification: (name: string) => `A tag you suggested for removal has been rejected on the document ${name}`,
     addCommentToDoc: (name: string) => `A new comment has been added to the document ${name}`,
     documentUpdate: (text: string, name: string) => `Document ${name}, has been updated ${text ? "with " + text : ""}`,
     invitePeopleDoc: (sharedUsers: string, role: string, name: string) => `Your document ${name} on CITIIS Management Platform, is shared with ${sharedUsers} in the capacity of ${role}`,
-    publishDocument: (name: string) => `Document ${name}, is published on CITIIS Management Platform`,
-    unPublishDocument: (name: string) => `Document ${name} is unpublished from CITIIS Management Platform.`,
-    replaceDocument: (name: string) => `Document ${name} on CITIIS Management Platform, is replaced with a new document.`,
+    publishDocument: (name: string) => `Document ${name}, is published`,
+    unPublishDocument: (name: string) => `Document ${name} is unpublished`,
+    replaceDocument: (name: string) => `Document ${name} is replaced with a new document.`,
     documentRequest: (name: string) => `Someone request your document for access on ${name}.`,
     documentRequestApproved: (name: string) => `your request for access was accecpt on his ${name} document.`,
     documentRequestRejected: (name: string) => `your request for access was rejected on his ${name} document.`,
@@ -18,7 +18,7 @@ export const DOC_NOTIFICATIONS = {
 }
 
 export const GROUP_NOTIFICATIONS = {
-    youAddTOGroup: `You are added to a user group [groupId], on CITIIS Management Platform.`,
+    youAddTOGroup: `You are added to a group [groupId]`,
     groupStatus: `Group status for [groupId], which you are a part of, has been updated on CITIIS Management Platform.`,
     addGroupMember: `A new member is added to the user group [groupId] on CITIIS Management Platform.`
 }
