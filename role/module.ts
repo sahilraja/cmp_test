89785edf4548ad6e5d2b2bb8849f141fd106e7ab
apoorva <apoorva.s@transerve.com>
2020-01-17 11:00:47 +0530

    removing special characters from role key

--- role/module.ts
+++ role/module.ts
@@ -305,16 +305,14 @@ export async function addRolesFromJSON() {
 
 export async function addRole(userId: string, bodyObj: any) {
     try {
-        let userRoles = await userRoleAndScope(userId);
-        let userRole = userRoles.data[0];
-        const isEligible = await checkRoleScope(userRole, "display-role-management");
-        if (!isEligible) {
-            throw new APIError("Unauthorized for this Action", 403);
-        }
+        // let userRoles = await userRoleAndScope(userId);
+        // let userRole = userRoles.data[0];
+        // const isEligible = await checkRoleScope(userRole, "display-role-management");
+        // if (!isEligible) {
+        //     throw new APIError("Unauthorized for this Action", 403);
+        // }
         if (!bodyObj.role || !bodyObj.category || !bodyObj.roleName) throw new Error("All mandatory fields are reuired")
-        const regex ='^[a-zA-Z0-9]+$'
-        // let role = bodyObj.role.replace(/ /g, '-'); 
-        let role = bodyObj.role.replace(regex, '')      
+        let role = bodyObj.role.replace(/[^a-zA-Z0-9]/g, "")      
         role = role.toLowerCase().trim()
         let response = await roleSchema.create({
             role: role,
